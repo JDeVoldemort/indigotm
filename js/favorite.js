419bf3f35c54b73f096d4739dc31c39c4bad3f84
JDVoldemort <dragonlore8@gmail.com>
2023-03-24 15:33:14 -0600

    api cache working

---
+++ js/favorite.js
@@ -0,0 +1,10 @@
+// add local storage item in on region pages with json to store a fav field
+// also local storage item to store favorites which will iterate through the pokemon info 
+// and save info if the fav field is included.
+// field to include in local item user-fav: {user: username { pokemon.name.type1 and if 2 then 2, image link, fav, on team 1 true 0 false }
+// code to remove fav from users local storage fav item
+
+// teams for pokemon in fav if on team then add element with phone icon to pokemon listed in fav 
+
+// code to display favs from local storage with on team icon and fav removal icon.
+// possible display alert when added or removed
--- js/pokeRegion.js
+++ js/pokeRegion.js
@@ -1,3 +1,4 @@
+import { preloadImages } from "./imageCache.mjs";
 // Kanto region Pokemon list number 1-151
 const urlListKanto = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0";
 const urlListJohto = "https://pokeapi.co/api/v2/pokemon?offset=151&limit=100";
@@ -11,19 +12,22 @@ let url = "";
 
 async function insertResults(data) {
     console.log(data);
-    reset();
+    // reset();
     const pokeListElement = document.querySelector(".outputList");
     let pokeList = data.results;
+    let imgArray = [];
+
     // const filterLetter = document.querySelector(".sortLetter").value;
     //  pokeList = filteredPokemon(pokeList);
     pokeList.forEach(async(currentItem) => {
       let url = currentItem.url;
       // let swap = url.substr(-2);
-      let name = currentItem.name;
+    //   let name = currentItem.name;
       // url = url.replace(swap, name);
       const pokedetails = await getPokemon(url);
       // if (response.ok)
       const pokemon = pokedetails;
+      
     //   if (filterLetter.length > 0 && filterLetter.length < 2) {
     //     if (name.charAt(0) != filterLetter) { console.log(filterLetter);
     //       return;
@@ -33,18 +37,25 @@ async function insertResults(data) {
         // }
     //   } else {
           // console.log(pokemon);
-        const html = `<p><img src='${pokemon.sprites.front_default}' alt='image of ${pokemon.name}'>${pokemon.name}</p>`;
+        imgArray.push(pokemon.sprites.front_default);
+        //   📲🤍💚
+        let html = `<div><img src='${pokemon.sprites.front_default}' alt='image of ${pokemon.name}'><span class="fav">🤍</span><p class="name">${pokemon.name} Types: ${pokemon.types[0].type.name} `;
+        if (pokemon.types[1] != undefined) {
+        html += `, ${pokemon.types[1].type.name}`;
+        }
+        html += `</p></div>`;
         pokeListElement.innerHTML += html;
     //   }
     });
+    preloadImages(imgArray, true);
   }
   function reset() {
     document.querySelector(".outputList").innerHTML = "";
   }
   
   // asyc fetch function including if.ok and send to a .json
-  async function getPokemonList(urlList) {
-    const response = await fetch(urlList);
+  async function getPokemonList(urlListKanto) {
+    const response = await fetch(urlListKanto);
     if (response.ok) {
       const dataA = await response.json();
       insertResults(dataA);
@@ -64,9 +75,9 @@ async function insertResults(data) {
   function callPokemonList() {
       getPokemonList(urlList);
   }
-  getPokemonList(urlList);
-  const button = document.querySelector('.filterButton');
-  header = document.querySelector('.input');
-  console.log(button);
-  button.addEventListener('click', callPokemonList);
+  getPokemonList(urlListKanto);
+//   const button = document.querySelector('.filterButton');
+//   header = document.querySelector('.input');
+//   console.log(button);
+//   button.addEventListener('click', callPokemonList);
 
