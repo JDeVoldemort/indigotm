50ff516c4bf6815bc48a5726854b91c644ef0d22
JDeVoldemort <dragonlore8@gmail.com>
2023-03-31 19:42:50 -0600

    fix and create JSON

--- js/pokeRegion.js
+++ js/pokeRegion.js
@@ -6,11 +6,25 @@ loadHeaderFooter();
 
 // Kanto region Pokemon list number 1-151
 const urlListTest = "https://pokeapi.co/api/v2/pokemon?limit=10&offset=0";
+
 const urlListKanto = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0";
+// Johto region Pokemon list number 152-251
+
 const urlListJohto = "https://pokeapi.co/api/v2/pokemon?offset=151&limit=100";
+// Hoenn region Pokemon list number 251-386
+
 const urlListHoenn = "https://pokeapi.co/api/v2/pokemon?offset=251&limit=135";
 
+// Pull to create redueced list of data to store in JSON pokeReducedList
+// Next lookup how to json.parse the reducedList
+const urlListFullTestPull = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=386";
+
 getPokemonList(urlListTest);
+// getPokemonList(urlListFullTestPull);
+// getPokemonList(urlListKanto);
+// getPokemonList(urlListJohto);
+// getPokemonList(urlListHoenn);
+
 
 // maybe turn these into an array
 // or read from jsons setup instead
